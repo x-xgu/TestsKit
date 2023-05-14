18f398f49469ae9d9d251e73ccbc97a48668ba55
g_xin <g_xin@outlook.com>
2023-05-14 13:49:37 +0800

    NEW: socket

--- testskit/conf/protocol/socket.py
+++ testskit/conf/protocol/socket.py
@@ -6,14 +6,30 @@ class SocketConfig:
     Send UDP messages to a socket.
     """
 
-    def __init__(self, ip: str, port: int):
-        self._ip = ip
-        self._port = port
+    def __init__(
+            self,
+            *,
+            ip: str,
+            port: int
+    ):
+        self.addr = (ip, port)
+        self.socket = None
 
-    def send_msg(self, msg) -> None:
-        addr = (self._ip, self._port)
+    def open_socket(self):
+        self.socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_DGRAM
+        )
+
+    def send_msg(self, msg: str) -> None:
+        """
+        Send message to a socket.
+        """
         with socket.socket(
                 socket.AF_INET,
                 socket.SOCK_DGRAM
         ) as s:
-            s.sendto(msg.encode(), addr)
+            s.sendto(
+                msg.encode(),
+                self.addr
+            )
