00a8dbc39807af203a3424ea68e1d0450cbd5a4b
xgu <g_xin@outlook.com>
2023-05-17 10:01:58 +0800

    NEW: file convert

--- selenite/common/convert.py
+++ selenite/common/convert.py
@@ -4,33 +4,81 @@ import cv2
 import numpy as np
 
 
-def zip_dict(keys: List[str], *values) -> List[Dict]:
+def zip_dict(keys: List[str], *values: List) -> List[Dict[str, List]]:
     """
     Merge keys and values into a list of dictionaries
+
+    Args:
+        keys (List[str]): A list of strings representing the keys for the dictionaries
+        *values (List): A variable number of arguments representing the values for the dictionaries
+
+    Returns:
+        List[Dict[str, List]]: A list of dictionaries with the keys and values merged together
+
+    Example:
+        >>> zip_dict(['name', 'age'], ['John', 25], ['Jane', 30])
+        [{'name': ['John'], 'age': [25]}, {'name': ['Jane'], 'age': [30]}]
     """
+
     dict_list = []
-    [dict_list.append(dict(zip(keys, value))) for value in values]
+    for value in values:
+        dict_list.append(dict(zip(keys, value)))
     return dict_list
 
 
-def format_decimal_number_with_commas(number):
+def format_decimal_number_with_commas(number: float) -> str:
     """
     Format decimal number with commas
+
+    Args:
+        number (float): A decimal number to be formatted with commas
+
+    Returns:
+        str: A string representation of the formatted decimal number
+
+    Example:
+        >>> format_decimal_number_with_commas(1234567.89)
+        '1,234,567.89'
     """
-    formatted_number = f'{float(number):,}'
+
+    formatted_number = f'{number:,.2f}'
     return formatted_number
 
 
-def bytes_to_numpy(bytes_data: bytes):
+def bytes_to_numpy(bytes_data: bytes) -> np.ndarray:
     """
     Convert bytes to numpy
+
+    Args:
+        bytes_data (bytes): A bytes object to be converted to a numpy array
+
+    Returns:
+        np.ndarray: A numpy array representing the image data
+
+    Example:
+        >>> with open('image.jpg', 'rb') as f:
+        ...     image_bytes = f.read()
+        ...
+        >>> image_data = bytes_to_numpy(image_bytes)
     """
+
     nparr = np.frombuffer(bytes_data, np.uint8)
     return cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
 
 
-def convert_sec_to_ms(timeout):
+def convert_sec_to_ms(timeout: float) -> float:
     """
     Convert seconds to milliseconds
+
+    Args:
+        timeout (float): A timeout value in seconds
+
+    Returns:
+        float: The timeout value converted to milliseconds
+
+    Example:
+        >>> convert_sec_to_ms(2.5)
+        2500.0
     """
+
     return timeout * 1000
--- selenite/common/file.py
+++ selenite/common/file.py
@@ -1,6 +1,6 @@
 import csv
 import json
-from typing import Any
+from typing import Any, List
 
 import yaml
 
@@ -8,6 +8,16 @@ import yaml
 def load_json(file_path: str) -> Any:
     """
     Load json file from project
+
+    Args:
+        file_path (str): The path of the json file to be loaded.
+
+    Returns:
+        Any: The loaded json data.
+
+    Example:
+        >>> load_json('data.json')
+        {'name': 'John', 'age': 30, 'city': 'New York'}
     """
     with open(file_path) as f:
         json_data = json.load(f)
@@ -17,15 +27,35 @@ def load_json(file_path: str) -> Any:
 def load_yml(file_path: str) -> Any:
     """
     Load yml file from project
+
+    Args:
+        file_path (str): The path of the yml file to be loaded.
+
+    Returns:
+        Any: The loaded yml data.
+
+    Example:
+        >>> load_yml('data.yml')
+        {'name': 'John', 'age': 30, 'city': 'New York'}
     """
     with open(file_path) as f:
         yml_data = yaml.safe_load(f)
     return yml_data
 
 
-def load_csv(file_path: str) -> Any:
+def load_csv(file_path: str) -> List[List[str]]:
     """
     Load csv file from project and return list of lists
+
+    Args:
+        file_path (str): The path of the csv file to be loaded.
+
+    Returns:
+        List[List[str]]: The loaded csv data as a list of lists.
+
+    Example:
+        >>> load_csv('data.csv')
+        [['name', 'age', 'city'], ['John', '30', 'New York'], ['Jane', '25', 'Los Angeles']]
     """
     data = []
     with open(file_path, newline='') as f:
