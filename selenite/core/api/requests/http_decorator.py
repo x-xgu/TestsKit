8d2f34cdacabc0f6f2eb014d7d9fb402ac26be58
xgu <g_xin@outlook.com>
2023-05-17 11:21:27 +0800

    NEW: http decorator

--- selenite/core/api/requests/http_decorator.py
+++ selenite/core/api/requests/http_decorator.py
@@ -19,12 +19,26 @@ COMMON_HEADERS = {
 
 
 class HttpDecorator:
+    """
+    A decorator class that sends HTTP requests to a specified URL.
+
+    Args:
+        url (str): The URL to send the request to.
+        method (RequestMethod, optional): The HTTP method to use. Defaults to 'get'.
+
+    Example:
+        >>> @HttpDecorator(url='https://jsonplaceholder.typicode.com/posts/1')
+        >>> def get_post():
+        >>>     return {'params': {'userId': 1}}
+        >>>
+        >>> response = get_post()
+    """
 
     def __init__(
             self,
             url: str,
             method: RequestMethod = 'get'
-    ):
+    ) -> None:
         self.url = url
         self.method = method
         self.func_return = {}
@@ -34,6 +48,22 @@ class HttpDecorator:
             self,
             func: Callable
     ) -> Callable:
+        """
+        The decorator function that sends the HTTP request.
+
+        Args:
+            func (Callable): The function to decorate.
+
+        Returns:
+            Callable: The decorated function.
+
+        Example:
+            >>> @HttpDecorator(url='https://jsonplaceholder.typicode.com/posts/1')
+            >>> def get_post():
+            >>>     return {'params': {'userId': 1}}
+            >>>
+            >>> response = get_post()
+        """
         self.func = func
         self.is_class = False
 
@@ -46,6 +76,26 @@ class HttpDecorator:
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            """
+            The wrapper function that sends the HTTP request.
+
+            Args:
+                *args: The positional arguments to pass to the decorated function.
+                **kwargs: The keyword arguments to pass to the decorated function.
+
+            Returns:
+                requests.Response: The response from the HTTP request.
+
+            Example:
+                >>> @HttpDecorator(url='https://jsonplaceholder.typicode.com/posts/1')
+                >>> def get_post():
+                >>>     return {'params': {'userId': 1}}
+                >>>
+                >>> response = get_post()
+
+            Raises:
+                RequestException: If the request fails.
+            """
             self.func_return = self.func(*args, **kwargs) or {}
             self.func_im_self = args[0] if self.is_class else object
 
@@ -67,12 +117,24 @@ class HttpDecorator:
         return wrapper
 
     def _create_url(self) -> str:
+        """
+        Creates the URL to send the request to.
+
+        Returns:
+            str: The URL to send the request to.
+        """
         base_url = getattr(self.func_im_self, 'base_url', '')
         url = self.func_return.pop('url', None) or self.url
 
         return ''.join([base_url, url])
 
     def _get_session(self) -> dict:
+        """
+        Gets the session information for the request.
+
+        Returns:
+            dict: The session information for the request.
+        """
         headers = getattr(self.func_im_self, 'header', {})
         headers.update(COMMON_HEADERS)
 
